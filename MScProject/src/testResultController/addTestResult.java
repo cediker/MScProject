36e3adfa33bccf1d68588e8767025a4fb641e859
Chrissie Ediker <chrissie.ediker@live.co.uk>
2019-09-09 13:11:38 +0100

    HTTP decode attempt

--- MScProject/src/testResultController/addTestResult.java
+++ MScProject/src/testResultController/addTestResult.java
@@ -1,6 +1,8 @@
 package testResultController;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -29,21 +31,46 @@ import modelTestResult.TestResultDAO;
 		}
 			 public void doPost(HttpServletRequest request,
 					HttpServletResponse response) throws ServletException, IOException {	 
-				//declares variables and sets them to null
 				
+				//declares variables and sets them to null
 				System.out.println("Production Standard: " + request.getParameter("value"));
 				System.out.println("Job Number: " + request.getParameter("jobNumber"));
 				System.out.println("Roll : " + request.getParameter("roll"));
 				//CreateQRCode.createQRCode(request.getParameter("value"));
-				 
 				
-				System.out.println(request.getParameter("items[0][name]"));
+				//gets the HTTP encoded test parameter String
+				String tr =	request.getParameter("testResult").toString();
 				
+				//split the URL on &
+				String[] parts = tr.split("&");
+				
+				//declares array list
+				ArrayList<String> myArray= new ArrayList();
+				
+				//loops through parts array to decode each section
+				for (String part : parts) {
+					
+				    try {
+				    	//decodes the URL split data
+					    String test = java.net.URLDecoder.decode(part, StandardCharsets.UTF_8.name());
+					    myArray.add(test);
+					    
+					} catch (UnsupportedEncodingException e) {   
+					}  
+				}
+				//prints array list to console
+				System.out.println("My Array List "+myArray);
+				
+				
+				
+				
+						
+			/*	
 				Enumeration<String> params = request.getParameterNames(); 
 				while(params.hasMoreElements()){
 				 String paramName = params.nextElement();
 				 System.out.println("Parameter Name - "+paramName+", Value - "+request.getParameter(paramName));
-				}
+				}*/
 			
 			 }
 		}
